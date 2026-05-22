74e8bbe826e9af489113e7fc323e5f51587bac11
Gabriel Brisolara <dev.brisolara@gmail.com>
2026-05-22 15:16:10 -0300

    feat: initialize WhatsLeft API with Serilog, Elasticsearch integration, and Helm deployment support

--- src/WhatsLeft.API/Program.cs
+++ src/WhatsLeft.API/Program.cs
@@ -1,5 +1,36 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var elasticUri = configuration["ElasticConfiguration:Uri"];
+if (!string.IsNullOrEmpty(elasticUri))
+{
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+        {
+            AutoRegisterTemplate = true,
+            IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+            NumberOfReplicas = 1,
+            NumberOfShards = 2
+        })
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+
+    builder.Host.UseSerilog();
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
